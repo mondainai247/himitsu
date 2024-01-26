eb201dfc2f3429957da6fd9cc7e3f56eb3aabf85
Frank <60919552+mondainai247@users.noreply.github.com>
2024-01-25 20:27:29 -0800

    Create himitsu_install.sh
    Created an install script

---
+++ himitsu_install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install build-essential -y
+sudo apt install gcc -y
+sudo apt install wget -y
+sudo apt install curl -y
+
+#On Linux
+sudo wget https://go.dev/dl/go1.21.5.linux-amd64.tar.gz
+
+#unpack it
+sudo tar -C /usr/local -xzf go1.21.5.linux-amd64.tar.gz
+
+
+export GOROOT=/usr/local/go
+export PATH=$PATH:$GOROOT/bin
+export PATH=$PATH:~/go/bin
+
+source ~/.bashrc
+go env
+#should output golang 1.19
+
+curl https://get.ignite.com/cli@v0.26.1 | bash
+
+curl https://get.ignite.com/cli | bash
+
+sudo mv ignite /usr/local/bin
+ignite version
+echo “Welome to Himitsu” 
+
+
+
+git clone https://github.com/mondainai247/himitsu.git
+
+cd himitsu
+make install 
+
+himitsud --vesion
